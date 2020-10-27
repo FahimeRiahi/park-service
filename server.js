db671ff10a58c12f7cbe78945491dfdbe474f6ef
fahimeriahi@gmail.com <fahimeriahi@gmail.com>
2020-10-28 00:44:36 +0330

    deploy config

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/pizza-delivery'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/park-service/index.html'));
+});
+// app.get('/', function(req, res){
+//   res.redirect('/todo');
+// });
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
