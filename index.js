219dcfe24e15563d93e99af139313f17c35b4c75
fahimeriahi@gmail.com <fahimeriahi@gmail.com>
2020-10-28 01:53:24 +0330

    deploy config

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const { GraphQLServer } = require('graphql-yoga')
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const http = require('http')
+const path = require('path');
 
 const app = express();
-app.use(bodyParser.json());
 
-// Create link to Angular build directory
-const distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+const port = process.env.PORT || 5000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
+
 const Query = require('./resolvers/query.js')
 const Mutation = require('./resolvers/mutation.js')
 
@@ -18,7 +26,7 @@ const resolvers = {
   Mutation,
 }
 
-const server = new GraphQLServer({
+const graphQLServer = new GraphQLServer({
   typeDefs: './schema.graphql',
   resolvers,
   context: request => {
@@ -31,5 +39,5 @@ const server = new GraphQLServer({
 
 
 const PORT = process.env.PORT || 3000
-server.start(PORT, () => console.log(`Server is running on http://localhost:4000`))
+graphQLServer.start(PORT, () => console.log(`Server is running on http://localhost:4000`))
 
